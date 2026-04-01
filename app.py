77790f4f1ca101a92e9a473aed9e5b9ce6f9fd7a
Renad <renadalbshri7@gmail.com>
2026-04-01 16:06:03 +0300

    Update birthday message to include 'Joey'

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ components.html(
     </head>
     <body>
 
-        <h2>🎂 Happy Birthday 🎂</h2>
+        <h2> Happy Birthday Joey  </h2>
         <div id="cake"></div>
         <p>Click on the cake to place candles</p>
         <p>Then blow into your mic 😘</p>
